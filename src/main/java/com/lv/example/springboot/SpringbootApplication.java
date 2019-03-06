ce263cbe579a75b2c8d5d0ab502cc7a820c99950
顺· 吕 <lvshun0932@163.com>
2019-03-06 09:59:48 +0800

    注释

--- src/main/java/com/lv/example/springboot/SpringbootApplication.java
+++ src/main/java/com/lv/example/springboot/SpringbootApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * run
+ */
 @SpringBootApplication
 @EnableSwagger2
 public class SpringbootApplication {
